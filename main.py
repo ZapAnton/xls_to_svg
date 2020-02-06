de0e09ce4270d4b22e463ec4ef781dffeb7e6e90
ZapAnton <wc3ft2@gmail.com>
2020-02-06 17:17:54 +0300

    Implemented the discrete_bar_chart type

--- main.py
+++ main.py
@@ -1,48 +1,4 @@
-from typing import List, Dict
-import numpy as np
-import matplotlib.pyplot as plt
 from xls_to_svg.cli import parse_cli_arguments, handle_arguments
-
-
-def plot_svg(results, category_names):
-    plt.clf()
-    labels = list(results.keys())
-    data = np.array(list(results.values()))
-    data_cum = data.cumsum(axis=1)
-    category_colors = plt.get_cmap('RdYlGn')(np.linspace(
-        0.15, 0.85, data.shape[1]))
-
-    fig, ax = plt.subplots(figsize=(9.2, 5))
-    ax.invert_yaxis()
-    ax.xaxis.set_visible(False)
-    ax.set_xlim(0, np.sum(data, axis=1).max())
-
-    for i, (colname, color) in enumerate(zip(category_names, category_colors)):
-        widths = data[:, i]
-        starts = data_cum[:, i] - widths
-        ax.barh(labels,
-                widths,
-                left=starts,
-                height=0.5,
-                label=colname,
-                color=color)
-        xcenters = starts + widths / 2
-
-        r, g, b, _ = color
-        text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
-        for y, (x, c) in enumerate(zip(xcenters, widths)):
-            ax.text(x,
-                    y,
-                    str(int(c)),
-                    ha='center',
-                    va='center',
-                    color=text_color)
-    ax.legend(ncol=len(category_names),
-              bbox_to_anchor=(0, 1),
-              loc='lower left',
-              fontsize='small')
-
-    plt.savefig('test.svg')
 
 
 if __name__ == '__main__':
--- xls_to_svg/chart_plotter.py
+++ xls_to_svg/chart_plotter.py
@@ -1,18 +1,84 @@
-from typing import List
 from pathlib import Path
+from typing import List, Dict, Callable
+import numpy as np
+import matplotlib.pyplot as plt
 from xls_to_svg.input_file_data import InputFileData
 
 
 class ChartPlotter:
 
     CHART_TYPES: List[str] = [
-        'bar_chart', 'stacked_bar_chart', 'pie_chart', 'donut_chart'
+        'bar_chart', 'discrete_bar_chart', 'stacked_bar_chart', 'pie_chart',
+        'donut_chart'
     ]
 
     def __init__(self, chart_type: str, xls_filepath: Path):
         self.chart_type: str = chart_type
         self.xls_filepath: Path = xls_filepath
+        self.__plot_method_mapping: Dict[str, Callable] = {
+            'bar_chart': self.__plot_bar_chart,
+            'discrete_bar_chart': self.__plot_discrete_bar_chart,
+            'stacked_bar_chart': self.__plot_stacked_bar_chart,
+            'pie_chart': self.__plot_pie_chart,
+            'donut_chart': self.__plot_donut_chart,
+        }
+
+    def __plot_bar_chart(self, input_data: InputFileData):
+        raise NotImplementedError()
+
+    def __plot_discrete_bar_chart(self, input_data: InputFileData):
+        plt.clf()
+        labels = list(input_data.data.keys())
+        data = np.array(list(input_data.data.values()))
+        data_cum = data.cumsum(axis=1)
+        category_colors = plt.get_cmap('RdYlGn')(np.linspace(
+            0.15, 0.85, data.shape[1]))
+
+        fig, ax = plt.subplots(figsize=(9.2, 5))
+        ax.invert_yaxis()
+        ax.xaxis.set_visible(False)
+        ax.set_xlim(0, np.sum(data, axis=1).max())
+
+        for i, (colname,
+                color) in enumerate(zip(input_data.categories,
+                                        category_colors)):
+            widths = data[:, i]
+            starts = data_cum[:, i] - widths
+            ax.barh(labels,
+                    widths,
+                    left=starts,
+                    height=0.5,
+                    label=colname,
+                    color=color)
+            xcenters = starts + widths / 2
+
+            r, g, b, _ = color
+            text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+            for y, (x, c) in enumerate(zip(xcenters, widths)):
+                ax.text(x,
+                        y,
+                        str(int(c)),
+                        ha='center',
+                        va='center',
+                        color=text_color)
+        ax.legend(ncol=len(input_data.categories),
+                  bbox_to_anchor=(0, 1),
+                  loc='lower left',
+                  fontsize='small')
+        chart_filepath: Path = self.xls_filepath.parent / (
+            self.xls_filepath.stem + '.svg')
+        plt.savefig(chart_filepath)
+
+    def __plot_stacked_bar_chart(self, input_data: InputFileData):
+        raise NotImplementedError()
+
+    def __plot_pie_chart(self, input_data: InputFileData):
+        raise NotImplementedError()
+
+    def __plot_donut_chart(self, input_data: InputFileData):
+        raise NotImplementedError()
 
     def plot(self):
         input_data = InputFileData.from_file(self.xls_filepath)
-        print(input_data)
+        plot_method: Callable = self.__plot_method_mapping[self.chart_type]
+        plot_method(input_data)
